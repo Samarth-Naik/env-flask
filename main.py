f71183c8d03aeaf95bcb2012aeed77e935d1c0ee
Samarth-Naik <naiksamarth124@gmail.con.com>
2024-10-15 10:10:58 +0530

    updated

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def display_env_var():
     # Get the value of an environment variable (e.g., "MY_ENV_VAR")
-    env_var = os.getenv('MY_ENV_VAR', 'Environment variable not set')
+    env_var = os.getenv('MY_ENV_VAR')
     return f'The value of MY_ENV_VAR is: {env_var}'
 
 if __name__ == '__main__':
